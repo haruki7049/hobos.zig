542ad097eefc4e1a9103bb5d74f3f8d2ad890916
haruki7049 <tontonkirikiri@gmail.com>
2024-09-13 13:34:37 +0900

    hoge

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,8 @@
+const console = @import("console.zig");
+
 export fn _start() linksection(".text.boot") callconv(.Naked) noreturn {
+    console.setColors(.White, .Blue);
+    console.clear();
+
     while (true) {}
 }
