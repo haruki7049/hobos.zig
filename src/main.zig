8dae5694122bcb8c1c2430cc386e8e8ebe324b11
haruki7049 <tontonkirikiri@gmail.com>
2024-09-13 13:37:47 +0900

    fuga

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,9 @@ const console = @import("console.zig");
 export fn _start() linksection(".text.boot") callconv(.Naked) noreturn {
     console.setColors(.White, .Blue);
     console.clear();
+    console.putString("Hello, world");
+    console.setForegroundColor(.LightRed);
+    console.putChar('!');
 
     while (true) {}
 }
