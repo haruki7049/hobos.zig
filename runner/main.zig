d9c68e07e311e3eae89d1a8ecc9266384bb0771d
haruki7049 <tontonkirikiri@gmail.com>
2025-07-01 15:22:19 +0900

    Update

--- runner/main.zig
+++ runner/main.zig
@@ -5,13 +5,18 @@ const ArrayList = std.ArrayList;
 
 pub fn main() !void {
     var args: std.process.ArgIterator = std.process.args();
-    const path: []const u8 = args.next() orelse return error.MissingArgument;
+    _ = args.next() orelse return error.MissingArgument; // a binary path for runner itself
+    const kernel_path: []const u8 = args.next() orelse "./zig-out/lib/hobos.elf"; // This program uses the default value, "./zig-out/lib/hobos.elf"
+
+    if (std.mem.eql(u8, kernel_path, "")) {
+        @panic("Cannot get a kernel path passed to Qemu");
+    }
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() != .ok) @panic("leak");
     const allocator = gpa.allocator();
 
-    const argv = [_][]const u8{ "qemu-system-riscv32", "-machine", "virt", "-bios", "default", "-serial", "mon:stdio", "-kernel", path };
+    const argv = [_][]const u8{ "qemu-system-riscv32", "-machine", "virt", "-bios", "default", "-serial", "mon:stdio", "-kernel", kernel_path };
 
     // By default, child will inherit stdout & stderr from its parents,
     // this usually means that child's output will be printed to terminal.
