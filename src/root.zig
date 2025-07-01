bc470b33625ac803387a3b2fe2edfa71d3bbdfe7
haruki7049 <tontonkirikiri@gmail.com>
2025-07-01 15:49:14 +0900

    feat: Update

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,14 @@
-export fn _start() linksection(".text.boot") callconv(.Naked) noreturn {
+const console = @import("console.zig");
+
+export fn panic() void {}
+
+export fn _start() linksection(".text.boot") void {
+    @call(.auto, kmain, .{});
+
     while (true) {}
+}
+
+fn kmain() void {
+    console.initialize();
+    console.puts("Hello world!");
 }
