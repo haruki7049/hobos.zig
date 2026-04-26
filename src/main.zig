3f7075fed925935ac7ee0697b7a98601328a8d7a
haruki7049 <tontonkirikiri@gmail.com>
2026-04-26 18:19:41 +0900

    Update

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,36 @@
 const std = @import("std");
+const uefi = std.os.uefi;
+const GraphicsOutput = uefi.protocol.GraphicsOutput;
 
 pub fn main() noreturn {
-    _ = std.os.uefi.handle;
-    _ = std.os.uefi.system_table;
+    const graphics_output = locate_graphics_protocol().?;
+    const vram_addr = graphics_output.mode.frame_buffer_base;
+    const vram_byte_size = graphics_output.mode.frame_buffer_size;
+    var vram: []u32 = @as([*]u32, @ptrFromInt(vram_addr))[0 .. vram_byte_size / @sizeOf(u32)];
+    for (0..vram.len) |i| {
+        vram[i] = 0xffffff;
+    }
 
     while (true) {
         asm volatile ("hlt");
     }
+}
+
+const EfiVoid = u8;
+
+fn locate_graphics_protocol() ?*GraphicsOutput {
+    var gop: ?*uefi.protocol.GraphicsOutput = null;
+    const boot_services = uefi.system_table.boot_services orelse return null;
+
+    // Locate the protocol
+    const status = boot_services.locateProtocol(
+        &GraphicsOutput.guid,
+        null,
+        // The interface is returned as *?*anyopaque
+        @ptrCast(&gop),
+    );
+
+    if (status != .success) return null;
+
+    return gop;
 }
